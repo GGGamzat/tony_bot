568b1b51c33734e67f21551583b1a021d0b58685
GGGamzat <abdullayevgamzat@gmail.com>
2024-04-02 22:16:00 +0300

    admin_panel: part_1

--- main.py
+++ main.py
@@ -10,12 +10,25 @@ dp = Dispatcher(bot=bot)
 main = ReplyKeyboardMarkup(resize_keyboard=True)
 main.add('Доходы').add('Расходы')
 
+main_admin = ReplyKeyboardMarkup(resize_keyboard=True)
+main_admin.add('Доходы').add('Расходы').add('Админ-панель')
+
+admin_panel = ReplyKeyboardMarkup(resize_keyboard=True)
+admin_panel.add('Добавить доход').add('Добавить расход').add('Админ-панель')
+
 
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
 	await message.answer_sticker('CAACAgIAAxkBAAICAmXg6D2UYDRshrLCDXiNZIYT83vgAAIVKgAC7AABwUpA5EMJg_c8oTQE')
 	await message.answer(f'{message.from_user.first_name}, добро пожаловать, я ваш личный бухгалтер',
 						 reply_markup=main)
+	if message.from_user.id == int(os.getenv('ADMIN_ID')):
+		await message.answer(f'Вы авторизовались как администратор', reply_markup=main_admin)
+
+
+@dp.message_handler(commands=['id'])
+async def cmd_id(message: types.Message):
+	await message.answer(f'{message.from_user.id}')
 
 
 @dp.message_handler(text='Доходы')
@@ -28,15 +41,22 @@ async def contacts(message: types.Message):
 	await message.answer(f'История расходов')
 
 
-@dp.message_handler(content_types=['sticker'])
-async def check_sticker(message: types.Message):
-	await message.answer(message.sticker.file_id) # узнать id стикера
-	await bot.send_message(message.from_user.id, message.chat.id)
+@dp.message_handler(text='Админ-панель')
+async def contacts(message: types.Message):
+	if message.from_user.id == int(os.getenv('ADMIN_ID')):
+		await message.answer(f'Вы вошли в админ-панель', reply_markup=admin_panel)
+	else:
+		await message.reply('Я тебя не понимаю')
 
 
-@dp.message_handler(content_types=['document', 'photo'])
-async def check_sticker(message: types.Message):
-	await bot.forward_message()
+
+
+
+
+# @dp.message_handler(content_types=['sticker'])
+# async def check_sticker(message: types.Message):
+# 	await message.answer(message.sticker.file_id) # узнать id стикера
+# 	await bot.send_message(message.from_user.id, message.chat.id)
 
 
 @dp.message_handler()
